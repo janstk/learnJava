e31e1fbea1043b50261c6050f078a79b287005d4
janstk <janstk@163.com>
2015-03-08 13:45:38 +0800

    arayyListTest..NoSuchElementException

--- java180/ArrayListExam.java
+++ java180/ArrayListExam.java
@@ -9,15 +9,26 @@ class ArrayListExam
 		li.add("c");
 		li.add("a");
 		li.add("c");
+		// Iterator it = li.iterator();
+		// while(it.hasNext())
+		// {
+		// 	sop(it.next()+"....."+it.next());
+		// }
+		//it will have an exception named NoSuchElementException...
+		sop(li);
 		System.out.println(removeX2(li));
 	}
 	public static ArrayList removeX2(ArrayList li)
 	{
 		ArrayList result = new ArrayList();
 		for (Object elem : li) {
-			if(result.indexOf(elem)<0)
+			if(!result.contains(elem))
 				result.add(elem);
 		}
 	return result;
+	}
+	public static void sop(Object obj)
+	{
+		System.out.println(obj);
 	}
 }
